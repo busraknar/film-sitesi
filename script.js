a140115b470af55f422df01886710935ae0687a8
busraknar <b.ertas91@gmail.com>
2024-08-06 00:33:38 +0300

    4 birim ötele

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const arrows= document.querySelectorAll(".arrow");
+const arrows = document.querySelectorAll(".arrow");
 // console.log(arrows); 1.53 sonrası
-const movieLists= document.querySelectorAll(".movie-list");
+const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow, i)=>{
-   arrow.addEventListener("click", function(){
-    movieLists[i].style.transform = `translateX(${movieLists[i].
-    computedStyleMap().get("transform")[0].x.value}px)`
-});   //alt+ 96, alt gr + virgül  //backtick keyword
-  
+arrows.forEach((arrow, i) => {
+   let clickCounter = 0;
+   const imageItem = movieLists[i].querySelectorAll("img").length;
+   arrow.addEventListener("click", function () {
+      
+      clickCounter++;  //tıklandıktan sonra
+
+      if (imageItem - (4 + clickCounter) >= 0) {
+
+         movieLists[i].style.transform = `translateX(${movieLists[i].
+            computedStyleMap().get("transform")[0].x.value - 300}px)`
+      } else {
+         movieLists[i].style.transform = "translateX(0)" }
+     
+      });   //alt+ 96, alt gr + virgül  //backtick keyword
 
    //console.log(movieLists[i].computedStyleMap().get("transform")[0].x.value);
 });

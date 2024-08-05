326f506b12261f5620178df3cc453a1c1f420885
busraknar <b.ertas91@gmail.com>
2024-08-06 00:06:50 +0300

    Ok edited

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const movieLists= document.querySelectorAll(".movie-list");
 arrows.forEach((arrow, i)=>{
    arrow.addEventListener("click", function(){
     movieLists[i].style.transform = `translateX(${movieLists[i].
-    computedStyleMap().get("transform")[0].x.value}
+    computedStyleMap().get("transform")[0].x.value}px)`
 });   //alt+ 96
 
 
    //console.log(movieLists[i].computedStyleMap().get("transform")[0].x.value);
-
-
 });
+
+
